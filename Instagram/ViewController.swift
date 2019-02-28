4817abbae35c428c2d5b4c205862353765047403
Tsuyoshi Furuhata <tfuruhat@hotmail.com>
2019-02-28 23:16:12 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by ふるふる on 2019/02/28.
+//  Copyright © 2019 Tsuyoshi Furuhata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
