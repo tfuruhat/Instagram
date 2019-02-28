0132f35b33dddf83757f8b14faf18b21e3523f92
Tsuyoshi Furuhata <tfuruhat@hotmail.com>
2019-03-01 00:21:06 +0900

    3章まで完了

---
+++ Instagram/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  Instagram
+//
+//  Created by ふるふる on 2019/03/01.
+//  Copyright © 2019 Tsuyoshi Furuhata. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
